a0a79f1464d07f9b10ee694a9c607126405605d2
dotnetDev-Revclerx <razzaniket84@gmail.com>
2024-06-26 13:59:06 +0530

    third change
    form aniket side

--- partialClass/partialClass/Program_AutoLogin.cs
+++ partialClass/partialClass/Program_AutoLogin.cs
@@ -24,6 +24,7 @@ namespace BirthdayProgram
                 while (!IsValidChoice(choice, 1, 2))
                 {
                     Console.WriteLine("Invalid choice. Please enter 1 or 2.");
+                    Console.WriteLine("hello maam.");
                     Console.Write("Choice: ");
                     choice = Console.ReadLine();
                 }
